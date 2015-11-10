fa7a907a110ce6bae8b4db191ab44bb2f0f9655b
Stuart Marquis <marquis@columbia.edu>
2015-11-10 11:45:18 -0500

    GeoBlacklight + GeoCombine + OpenGeoMetadata harvests + Capistrano

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = 'fd51ffe90e6c2023ae48078127b4d34e9d439cf72e5f1e98e00116c397f9b9f39b0bef0ec1d10c91ded821d2948454db7ae4f61cb64a4e6037a901d34fd25705'
+
